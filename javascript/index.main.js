ac08c8068a461c5ed7f3f20801d72a8ff13fb579
unknown <t.bakeer4@gmail.com>
2018-06-08 02:47:39 -0700

    style in index.html and display more book  details in search.js

---
+++ javascript/index.main.js
@@ -0,0 +1,24 @@
+var myInterval;
+myInterval = setInterval(displayQuote, 5000);
+
+function displayQuote()
+{
+  $.ajax({
+   url:'http://api.forismatic.com/api/1.0/',
+   jsonp:'jsonp',
+   dataType:'jsonp',
+   data:{
+   method:'getQuote',
+   lang:'en',
+   format:'jsonp'},
+   success:function(response){
+   console.log(response.quoteText);
+   
+    $("#quote").text(response.quoteText)
+   // $("#author").text(response.quoteAuthor)
+
+
+}
+
+  })
+}
--- javascript/search.js
+++ javascript/search.js
@@ -14,48 +14,80 @@ $("#search-btn").on("click", function (event) {
         var url = "https://www.googleapis.com/books/v1/volumes?Key=AIzaSyBiP1NMmkUqkbKrg1Q3B9ou9HvUvP-t5xc&q=" + q;
 
         axios.get(url).then(function (response) {
-            // console.log( response.data.items);
-             $("#book-view").empty(); 
-            for (var i = 0; i < response.data.items.length; i++) { 
-               
-              
+            //clear books div 
+            $("#book-view").empty();
+            //for every item in response
+            console.log(response.data.items.length)
+            if (response.data.items.length===0)
+            {
+                $("#book-view").text("no results");
+            }
+            else 
+            {
+            for (var i = 0; i < response.data.items.length; i++)
+             {
+                var rating;
                 //check if this empty then mention that to user 
                 var item = response.data.items[i];
-                   console.log(item.volumeInfo);
-                var isbn=item.volumeInfo.industryIdentifiers[1].identifier;
+
+                console.log(item.volumeInfo);
+                if(item.volumeInfo.industryIdentifiers)
+                {
+                var isbn = item.volumeInfo.industryIdentifiers[1].identifier;
+                
+                //call ajax for goodreads API using isbn
+
+                var url = "https://cors.io/?http://www.goodreads.com/book/review_counts.json?key=UyhBVmqCWPtrAZdeZOn51A&isbns=" + isbn;
+                axios.get(url)
+                    .then(function (response) {
+                        console.log(response)
+                        rating = response.data.books[0].average_rating;
+
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    })
+                }
                 var bookTitle = item.volumeInfo.title;
                 var imgSrc = item.volumeInfo.imageLinks.smallThumbnail;
+                if (item.volumeInfo.authors){
                 var authors = item.volumeInfo.authors[0];
+                }
+                if(item.volumeInfo.categories){
                 var category = item.volumeInfo.categories[0];
-                var desription = item.volumeInfo.description;
-               
+                }
 
-
-                console.log(category);
-
-                console.log(authors);
-
-
-
+                var description = item.volumeInfo.description;
+                var infolink=item.volumeInfo.infoLink;
+                console.log(infolink);
                 var bookImg = $("<img>");
                 bookImg.attr("src", imgSrc);
                 bookImg.css("width", "180px");
                 bookImg.css("height", "180px");
-                bookImg.addClass("book-img");
+               bookImg.append("<a href="+infolink+"></a>");
+                
+               
+               // var a=$("<a  target='_blank' href="+infolink+"></a>")
+              //  bookImg.wrap(a);
                 var titleP = $("<p>").text("Title :" + bookTitle);
-                var modalBtn = $("<button type='Button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Description</button>")
-                var col = $("<div class='col-md-3 col-sm-6 mt-2 '>").append([bookImg, titleP]);
+                if (authors){
+                var authorP = $("<p>").text("Author is  :" + authors);}
+                if(category){
+                var categoryP = $("<p>").text("Category is  :" + category);}
+                var ratingP = $("<p>").text("Rating is  :" + rating);
+                var button=$("<button class='btn btn-info btn-lg'> Desription</buton>")
+                button.attr("onClick", "bootbox.alert('"+description+"')");
+                var col = $("<div class='col-md-3 col-sm-6 mt-2 '>").append([bookImg, titleP, ratingP, authorP, categoryP,button]);
                 $("#book-view").append(col);
 
-                
-            //    bootbox.alert("Hello world!");
 
+               
 
 
 
 
             }
-
+        }
         }).catch(function (err) {
             console.error(err)
         })
