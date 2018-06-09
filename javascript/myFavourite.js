1c4a52177a178be42ea07f0d785ca431f4f4ca7e
unknown <t.bakeer4@gmail.com>
2018-06-09 02:51:26 -0700

    added login authentication ,still needs work

---
+++ javascript/myFavourite.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDddZ0MY3GQzW0dCAvEJGHzwN7KpdndyTc",
+    authDomain: "bookworm-f822f.firebaseapp.com",
+    databaseURL: "https://bookworm-f822f.firebaseio.com",
+    projectId: "bookworm-f822f",
+    storageBucket: "bookworm-f822f.appspot.com",
+    messagingSenderId: "361463431080"
+  };
+  firebase.initializeApp(config);
