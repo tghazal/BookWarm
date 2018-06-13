af8f2604dd0aab567d4e2ca5f6b26c84b2e6ab83
unknown <t.bakeer4@gmail.com>
2018-06-12 19:30:09 -0700

    validate login form

--- javascript/login.js
+++ javascript/login.js
@@ -2,50 +2,57 @@
 
 $("#registerBtn").on("click",function(event){
     event.preventDefault();
+    console.log ("hi")
+    $("#error2").empty();
     var name=$("#name1-input").val().trim();
     var email=$("#email1-input").val().trim();
     var password=$("#password1-input").val().trim();
-    if(!email || !password)
+    console.log(name);
+    if(name==="" || email==="" || password ==="")
     {
-        return console.log("email and Password required")
+        $("#error2").text("All fields are required")
+        return 
     }
+    else
+    {
    firebase.auth().createUserWithEmailAndPassword(email,password)
    .then(function()
   {
-   
     firebase.auth().currentUser.updateProfile({displayName:name})
-   // user.updateProfile({});
+   
   })
   .catch(function(error){
       console.log("error,"+error)
+      $("#error2").text(error.message);
   });
-//   firebase.auth().signInWithEmailAndPassword(email,password)
-//   .catch(function(error){
-//     console.log("error,"+error.message)
-//   });
-//   console.log("welcome"+firebase.auth().currentUser.displayName)
-//   var p=$("<p>").text("welcome"+firebase.auth().currentUser.displayName).
-//   $("#name").append(p);
-  //LogInUsers( firebase.auth().currentUser);
-   
+  
+    }
    }) 
+  
   
   $("#loginBtn").on("click",function(event){
     event.preventDefault();
-  
+    $("#error").empty();
     var email=$("#email2-input").val().trim();
     var password=$("#password2-input").val().trim();
-    
+    if( email==="" || password ==="")
+    {
+        $("#error").text("All fields are required")
+        return 
+    }
+    else{
   firebase.auth().signInWithEmailAndPassword(email,password).then(function(){
     bootbox.alert("welcome "+firebase.auth().currentUser.displayName)
-   // window.location.href = 'index.html';
+    window.location.href = 'index.html';
      //  alert("welcome"+firebase.auth().currentUser.displayName)
   })
   .catch(function(error){
+      
+    $("#error").text(error.message);
     console.log("error,"+error.message)
   });
 
-
+    }
   
     
    }) 
